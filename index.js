dfddb8dedd8624c729306a28b780fc9445decfe4
Christy Perozzi <christy.perozzi@gmail.com>
2021-10-12 11:18:22 -0400

    commit tree 1deed870ca69be61399178746b7f6d0310706984

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1= 2;
+let num2= 31;
+let num3=9;
+let num4=5;
+let multiply = num1 * num2;
+let random = Number.parseInt(100*Math.random());
+let mod = num3 % num4;
+let max = Math.max(1,2,20,3);
